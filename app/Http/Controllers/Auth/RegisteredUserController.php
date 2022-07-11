8b65cc7b0534c8f840e8834f3572bee41af6221f
oksanaozcan <mekansa83@gmail.com>
2022-07-11 14:49:18 +0300

    implement inertia with react

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -5,23 +5,23 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
-use App\Types\RoleType;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
+use Inertia\Inertia;
 
 class RegisteredUserController extends Controller
 {
     /**
      * Display the registration view.
      *
-     * @return \Illuminate\View\View
+     * @return \Inertia\Response
      */
     public function create()
     {
-        return view('auth.register');
+        return Inertia::render('Auth/Register');
     }
 
     /**
@@ -35,8 +35,8 @@ class RegisteredUserController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
@@ -44,7 +44,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-        ])->assignRole(RoleType::READER);
+        ]);
 
         event(new Registered($user));
 
--- routes/web.php
+++ routes/web.php
@@ -5,53 +5,61 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\TagController;
 use App\Http\Controllers\Admin\UserController;
 use App\Types\PermissionType;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware(['auth'])->group(function () {
   Route::prefix('admin')->group(function () {
-    Route::get('/', [AdminController::class, 'index'])->middleware(['can:'.PermissionType::CAN_CREATE_USER, ])->name('admin.index');
+    Route::get('/', [AdminController::class, 'index'])->middleware(['can:'.PermissionType::CAN_CREATE_USER])->name('admin.index');
 
     Route::prefix('users')->group(function () {
-      Route::get('/', [UserController::class, 'index'])->middleware(['can:'.PermissionType::CAN_CREATE_USER, ])->name('admin.user.index');
-      Route::get('/deleted', [UserController::class, 'indexDeleted'])->middleware(['can:'.PermissionType::CAN_CREATE_USER, ])->name('admin.user.deleted');
-      Route::get('/{user}', [UserController::class, 'show'])->middleware(['can:'.PermissionType::CAN_CREATE_USER, ])->name('admin.user.show');
-      Route::get('/{user}/edit', [UserController::class, 'edit'])->middleware(['can:'.PermissionType::CAN_UPDATE_USER, ])->name('admin.user.edit');
-      Route::patch('/{user}', [UserController::class, 'update'])->middleware(['can:'.PermissionType::CAN_UPDATE_USER, ])->name('admin.user.update');
-      Route::delete('/{user}', [UserController::class, 'delete'])->middleware(['can:'.PermissionType::CAN_DELETE_USER, ])->name('admin.user.delete');
+      Route::get('/', [UserController::class, 'index'])->middleware(['can:'.PermissionType::CAN_CREATE_USER])->name('admin.user.index');
+      Route::get('/deleted', [UserController::class, 'indexDeleted'])->middleware(['can:'.PermissionType::CAN_CREATE_USER])->name('admin.user.deleted');
+      Route::get('/{user}', [UserController::class, 'show'])->middleware(['can:'.PermissionType::CAN_CREATE_USER])->name('admin.user.show');
+      Route::get('/{user}/edit', [UserController::class, 'edit'])->middleware(['can:'.PermissionType::CAN_UPDATE_USER])->name('admin.user.edit');
+      Route::patch('/{user}', [UserController::class, 'update'])->middleware(['can:'.PermissionType::CAN_UPDATE_USER])->name('admin.user.update');
+      Route::delete('/{user}', [UserController::class, 'delete'])->middleware(['can:'.PermissionType::CAN_DELETE_USER])->name('admin.user.delete');
     });
 
     Route::prefix('categories')->group(function () {
-      Route::get('/', [CategoryController::class, 'index'])->middleware(['can:'.PermissionType::CAN_CREATE_USER, ])->name('admin.category.index');
-      Route::get('/deleted', [CategoryController::class, 'indexDeleted'])->middleware(['can:'.PermissionType::CAN_CREATE_USER, ])->name('admin.category.deleted');
-      Route::get('/create', [CategoryController::class, 'create'])->middleware(['can:'.PermissionType::CAN_CREATE_USER, ])->name('admin.category.create');
-      Route::post('/store', [CategoryController::class, 'store'])->middleware(['can:'.PermissionType::CAN_CREATE_USER, ])->name('admin.category.store');
-      Route::get('/{category}', [CategoryController::class, 'show'])->middleware(['can:'.PermissionType::CAN_CREATE_USER, ])->name('admin.category.show');
-      Route::get('/{category}/edit', [CategoryController::class, 'edit'])->middleware(['can:'.PermissionType::CAN_UPDATE_USER, ])->name('admin.category.edit');
-      Route::patch('/{category}', [CategoryController::class, 'update'])->middleware(['can:'.PermissionType::CAN_UPDATE_USER, ])->name('admin.category.update');
-      Route::delete('/{category}', [CategoryController::class, 'delete'])->middleware(['can:'.PermissionType::CAN_DELETE_USER, ])->name('admin.category.delete');
+      Route::get('/', [CategoryController::class, 'index'])->middleware(['can:'.PermissionType::CAN_CREATE_USER])->name('admin.category.index');
+      Route::get('/deleted', [CategoryController::class, 'indexDeleted'])->middleware(['can:'.PermissionType::CAN_CREATE_USER])->name('admin.category.deleted');
+      Route::get('/create', [CategoryController::class, 'create'])->middleware(['can:'.PermissionType::CAN_CREATE_USER])->name('admin.category.create');
+      Route::post('/store', [CategoryController::class, 'store'])->middleware(['can:'.PermissionType::CAN_CREATE_USER])->name('admin.category.store');
+      Route::get('/{category}', [CategoryController::class, 'show'])->middleware(['can:'.PermissionType::CAN_CREATE_USER])->name('admin.category.show');
+      Route::get('/{category}/edit', [CategoryController::class, 'edit'])->middleware(['can:'.PermissionType::CAN_UPDATE_USER])->name('admin.category.edit');
+      Route::patch('/{category}', [CategoryController::class, 'update'])->middleware(['can:'.PermissionType::CAN_UPDATE_USER])->name('admin.category.update');
+      Route::delete('/{category}', [CategoryController::class, 'delete'])->middleware(['can:'.PermissionType::CAN_DELETE_USER])->name('admin.category.delete');
     });
 
     Route::prefix('tags')->group(function () {
-      Route::get('/', [TagController::class, 'index'])->middleware(['can:'.PermissionType::CAN_CREATE_USER, ])->name('admin.tag.index');
-      Route::get('/deleted', [TagController::class, 'indexDeleted'])->middleware(['can:'.PermissionType::CAN_CREATE_USER, ])->name('admin.tag.deleted');
-      Route::get('/create', [TagController::class, 'create'])->middleware(['can:'.PermissionType::CAN_CREATE_USER, ])->name('admin.tag.create');
-      Route::post('/store', [TagController::class, 'store'])->middleware(['can:'.PermissionType::CAN_CREATE_USER, ])->name('admin.tag.store');
-      Route::get('/{tag}', [TagController::class, 'show'])->middleware(['can:'.PermissionType::CAN_CREATE_USER, ])->name('admin.tag.show');
-      Route::get('/{tag}/edit', [TagController::class, 'edit'])->middleware(['can:'.PermissionType::CAN_UPDATE_USER, ])->name('admin.tag.edit');
-      Route::patch('/{tag}', [TagController::class, 'update'])->middleware(['can:'.PermissionType::CAN_UPDATE_USER, ])->name('admin.tag.update');
-      Route::delete('/{tag}', [TagController::class, 'delete'])->middleware(['can:'.PermissionType::CAN_DELETE_USER, ])->name('admin.tag.delete');
+      Route::get('/', [TagController::class, 'index'])->middleware(['can:'.PermissionType::CAN_CREATE_USER])->name('admin.tag.index');
+      Route::get('/deleted', [TagController::class, 'indexDeleted'])->middleware(['can:'.PermissionType::CAN_CREATE_USER])->name('admin.tag.deleted');
+      Route::get('/create', [TagController::class, 'create'])->middleware(['can:'.PermissionType::CAN_CREATE_USER])->name('admin.tag.create');
+      Route::post('/store', [TagController::class, 'store'])->middleware(['can:'.PermissionType::CAN_CREATE_USER])->name('admin.tag.store');
+      Route::get('/{tag}', [TagController::class, 'show'])->middleware(['can:'.PermissionType::CAN_CREATE_USER])->name('admin.tag.show');
+      Route::get('/{tag}/edit', [TagController::class, 'edit'])->middleware(['can:'.PermissionType::CAN_UPDATE_USER])->name('admin.tag.edit');
+      Route::patch('/{tag}', [TagController::class, 'update'])->middleware(['can:'.PermissionType::CAN_UPDATE_USER])->name('admin.tag.update');
+      Route::delete('/{tag}', [TagController::class, 'delete'])->middleware(['can:'.PermissionType::CAN_DELETE_USER])->name('admin.tag.delete');
     });
 
   });
-});
 
+});
 
 require __DIR__.'/auth.php';
