d343cdd51fae661738c319afa8c536256d1e7dfc
oksanaozcan <mekansa83@gmail.com>
2022-07-19 14:32:52 +0300

    CommentsGrid section added

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -3,6 +3,7 @@ import { Link, Head } from '@inertiajs/inertia-react';
 import CategoryItem from '@/Components/CategoryItem';
 import Navbar from '@/Components/Navbar';
 import Footer from '@/Components/Footer';
+import CommentsGrid from '@/Components/CommentsGrid';
 
 export default function Welcome(props) {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,8 +19,28 @@ export default function Welcome(props) {
           <section id='hero'>
             <div className="container max-w-6xl mx-auto px-6 py-12">
               <Navbar isOpen={isOpen} navToggle={navToggle}/>
-              <div className='max-w-lg mt-32 mb-32 p-4 font-sans text-4xl text-white uppercase border-2 md:p-10 md:m-32 md:mx-0 md:text-6xl'>
-                This is some cool text
+              <div className='max-w-lg mt-32 mb-32 p-4 font-sans text-4xl text-white uppercase border-2 border-slate-500/50 md:p-10 md:m-32 md:mx-0 md:text-6xl bg-slate-900/50'>
+                {
+                  props.auth.user ? 
+                  (
+                    <div className='flex flex-col justify-center'>
+                      <div className='text-base my-2 text-center'>Welcome Back {props.auth.user.name}</div>
+                      <Link href={route('dashboard')} className="btn text-base my-2 text-center">
+                        Dashboard
+                      </Link>
+                    </div>                    
+                  ) : 
+                  (                    
+                    <div className='flex flex-col'>
+                      <Link href={route('login')} className="btn text-base my-2 text-center">
+                        Log in
+                      </Link>
+                      <Link href={route('register')} className="btn text-base my-2 text-center">
+                        Register
+                      </Link>
+                    </div>
+                  )
+                }
               </div>
             </div>
           </section>
@@ -29,7 +50,7 @@ export default function Welcome(props) {
               <img className='contrast-75' src="images/camper.jpg" alt="" />
               <div className="top-40 pr-0 bg-white md:absolute md:right-0 md:py-10 md:pl-20">
                 <h2 className='max-w-lg mt-10 mb-6 font-sans text-4xl text-center text-gray-900 uppercase md:text-5xl md:mt-0 md:text-left'>
-                The standard Lorem Ipsum passage, used since the 1500s
+                The standard Lorem Ipsum passage.
                 </h2>
                 <p className='max-w-md text-center md:text-left'>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
@@ -40,7 +61,7 @@ export default function Welcome(props) {
             </div>
           </section>
 
-          <section id='creations'>
+          <section id='categories'>
             <div className="container max-w-6xl mx-auto my-32 px-6 text-gray-900 md:px-0">
               <div className='flex justify-center mb-20 md:justify-between'>
                 <h2 className='text-4xl text-center uppercase md:text-left md:text-5xl'>
@@ -67,6 +88,10 @@ export default function Welcome(props) {
               <button className='btn w-full md:hidden'>See All</button>
               </div>
             </div>
+          </section>
+
+          <section id='comments'>
+            <CommentsGrid/>
           </section>
 
           <Footer/>
