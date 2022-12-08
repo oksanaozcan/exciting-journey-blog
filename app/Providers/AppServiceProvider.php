8f2adb87fafd6b8b657cc85d03a193a054874aff
oksanaozcan <mekansa83@gmail.com>
2022-12-08 09:12:17 +0300

    AppServiceProvider refactoring

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Category;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
